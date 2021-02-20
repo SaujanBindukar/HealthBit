b838a7d3cdcd54072dd5bbdd59ef68575915f515
Saujan Bindukar <sauzanbindukar@gmail.com>
2021-02-20 12:14:21 +0545

    overflow in medical history page fixed

--- lib/features/Patient/Medical_History/medicalHistory.dart
+++ lib/features/Patient/Medical_History/medicalHistory.dart
@@ -14,93 +14,95 @@ class _MedicalHistoryState extends State<MedicalHistory> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff5BA2F4),
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 12),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pop();
-                },
-                child: Icon(
-                  Icons.chevron_left,
-                  size: 40,
-                  color: Colors.white,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Icon(
+                    Icons.chevron_left,
+                    size: 40,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Medical history",
-                style: GoogleFonts.poppins(
-                  fontSize: 32,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Image.asset(
-                    "assets/profile.png",
-                    height: 80,
-                    width: 80,
+                Text(
+                  "Medical history",
+                  style: GoogleFonts.poppins(
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Kiran Pradhan",
-                        style: GoogleFonts.poppins(
-                          fontSize: 20,
-                          color: Colors.white,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      "assets/profile.png",
+                      height: 80,
+                      width: 80,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Kiran Pradhan",
+                          style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                      Text(
-                        "UserId",
-                        style: GoogleFonts.poppins(
-                          fontSize: 20,
-                          color: Colors.white,
+                        Text(
+                          "UserId",
+                          style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              //Medical record list
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                //Medical record list
 
-              CustomMedicalRecords(
-                recordId: "001",
-                doctorName: "Mr. Doctor",
-                hospitalName: "Hospital Name",
-                description:
-                    "Patient was diagnosed with diarrhea and STDs of all kinds.",
-                otherPrescription: "Stay away from cold foods.",
-                medicine: "D-cold",
-              ),
+                CustomMedicalRecords(
+                  recordId: "001",
+                  doctorName: "Mr. Doctor",
+                  hospitalName: "Hospital Name",
+                  description:
+                      "Patient was diagnosed with diarrhea and STDs of all kinds.",
+                  otherPrescription: "Stay away from cold foods.",
+                  medicine: "D-cold",
+                ),
 
-              CustomMedicalRecords(
-                recordId: "001",
-                doctorName: "Mr. Doctor",
-                hospitalName: "Hospital Name",
-                description:
-                    "Patient was diagnosed with diarrhea and STDs of all kinds.",
-                otherPrescription: "Stay away from cold foods.",
-                medicine: "D-cold",
-              ),
-            ],
+                CustomMedicalRecords(
+                  recordId: "001",
+                  doctorName: "Mr. Doctor",
+                  hospitalName: "Hospital Name",
+                  description:
+                      "Patient was diagnosed with diarrhea and STDs of all kinds.",
+                  otherPrescription: "Stay away from cold foods.",
+                  medicine: "D-cold",
+                ),
+              ],
+            ),
           ),
         ),
       ),
